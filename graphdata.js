53f6626a46c6c14717c0e66f210670d8506e5aa3
Jenkins <jenkins@pnnl.gov>
2019-06-21 08:29:47 -0700

    Performance Test Results

---
+++ graphdata.js
@@ -0,0 +1,23 @@
+// AUTOMATICALLY GENERATED GRAPH DESCRIPTION
+document.title = "Chapel Performance Graphs for puma";
+var pageTitle = "Chapel Performance Graphs for puma";
+var runDate= "2019-06-19";
+var numericX = false;
+var configurations = [];
+var configurationsVis = [];
+var allGraphs = [
+{
+   "title" : "CHGL Runtime Performance",
+   "suites" : [],
+   "datfname" : "CHGL0.json",
+   "ylabel" : "Time (seconds)",
+   "startdate" : "2019-06-17",
+   "enddate" : "2019-06-19",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+}
+];
+var perfSuites = [
+
+];
