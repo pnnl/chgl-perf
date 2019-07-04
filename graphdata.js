6e79f2d17cba70ac75cfd6d0e863c4efee850537
PNNL Dev Central <dev-central@pnnl.gov>
2019-07-03 18:44:29 -0700

    Performance Test Update

--- graphdata.js
+++ graphdata.js
@@ -1,7 +1,7 @@
 // AUTOMATICALLY GENERATED GRAPH DESCRIPTION
 document.title = "Chapel Performance Graphs for puma";
 var pageTitle = "Chapel Performance Graphs for puma";
-var runDate= "2019-07-01";
+var runDate= "2019-07-03";
 var numericX = false;
 var configurations = [];
 var configurationsVis = [];
@@ -12,7 +12,18 @@ var allGraphs = [
    "datfname" : "CHGL0.json",
    "ylabel" : "Time (seconds)",
    "startdate" : "2019-06-17",
-   "enddate" : "2019-07-01",
+   "enddate" : "2019-07-03",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+},
+{
+   "title" : "ActiveDNS Performance per Phase",
+   "suites" : [],
+   "datfname" : "activeDNS0.json",
+   "ylabel" : "Time (seconds)",
+   "startdate" : "2019-07-01",
+   "enddate" : "2019-07-03",
    "displayrange" : true,
    "defaultexpand" : false,
    "annotations" : []
