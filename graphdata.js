15d30316503365aa571008bcd0ded1afdc829690
PNNL Dev Central <dev-central@pnnl.gov>
2019-07-11 12:50:48 -0700

    Performance Test Update

--- graphdata.js
+++ graphdata.js
@@ -1,18 +1,40 @@
 // AUTOMATICALLY GENERATED GRAPH DESCRIPTION
 document.title = "Chapel Performance Graphs for puma";
 var pageTitle = "Chapel Performance Graphs for puma";
-var runDate= "2019-07-10";
+var runDate= "2019-07-11";
 var numericX = false;
 var configurations = [];
 var configurationsVis = [];
 var allGraphs = [
 {
-   "title" : "CHGL Runtime Performance",
+   "title" : "Erdos Renyi Hypergraph Generation (32K Vertices, 64K Edges, 200M Inclusions)",
    "suites" : [],
-   "datfname" : "CHGL0.json",
+   "datfname" : "ErdosRenyi0.json",
    "ylabel" : "Time (seconds)",
    "startdate" : "2019-06-17",
-   "enddate" : "2019-07-10",
+   "enddate" : "2019-07-11",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+},
+{
+   "title" : "Chung-Lu Hypergraph Generation (1M Vertices, 2M Edges, 10M Inclusions)",
+   "suites" : [],
+   "datfname" : "ChungLu0.json",
+   "ylabel" : "Time (seconds)",
+   "startdate" : "2019-06-17",
+   "enddate" : "2019-07-11",
+   "displayrange" : true,
+   "defaultexpand" : false,
+   "annotations" : []
+},
+{
+   "title" : "BTER Hypergraph Generation (100K Vertices, 200K Edges, 1M Inclusions)",
+   "suites" : [],
+   "datfname" : "BTER0.json",
+   "ylabel" : "Time (seconds)",
+   "startdate" : "2019-06-17",
+   "enddate" : "2019-07-11",
    "displayrange" : true,
    "defaultexpand" : false,
    "annotations" : []
@@ -23,7 +45,7 @@ var allGraphs = [
    "datfname" : "activeDNS0.json",
    "ylabel" : "Time (seconds)",
    "startdate" : "2019-07-09",
-   "enddate" : "2019-07-10",
+   "enddate" : "2019-07-11",
    "displayrange" : true,
    "defaultexpand" : false,
    "annotations" : []
